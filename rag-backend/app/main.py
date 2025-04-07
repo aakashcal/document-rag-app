19d524a831e21f7c5980a8d251f3ed52fb9e8357
Aakash Sharma <aakashsharma@Aakashs-MacBook-Pro.local>
2025-04-07 16:53:05 +0530

    All changes

--- rag-backend/app/main.py
+++ rag-backend/app/main.py
@@ -1,7 +1,8 @@
+#source venv/bin/activate 
 from fastapi import FastAPI
 
 app = FastAPI()
-
+# Root endpoint that returns a simple Hello World message
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
